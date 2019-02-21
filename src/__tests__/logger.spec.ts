169a229f4ed53d7ee5548d05a5a77601e95de589
Matt Penrice <matthew.penrice@gmail.com>
2019-02-21 14:02:41 +0000

    WIP: ready to build the processed records
    affects: partridge-config, partridge-importer, partridge-logging, partridge-models,
    partridge-schema, partridge-shell

--- src/__tests__/logger.spec.ts
+++ src/__tests__/logger.spec.ts
@@ -1,16 +1,16 @@
 import {Logger} from '../logger'
 import * as formatModule from '../format'
 import {Dumpables, LogOptions} from '../__types__'
+import {FeedSupplierName} from 'partridge-config'
 
 describe('logger', () => {
-
   const winstonLoggerMock: any = {log: jest.fn()}
   const logger = new Logger(winstonLoggerMock, 'warn')
   const formatter = jest.spyOn(formatModule, 'default')
   const axiosRequestMock = {
     url: 'http://bbc.com',
     headers: {
-      'x-feed-supplier': 'coral',
+      'x-feed-supplier': FeedSupplierName.coral,
     },
   }
 
@@ -32,24 +32,32 @@ describe('logger', () => {
     const dumpables: Dumpables = {axiosRequest: [axiosRequestMock]}
     const logOptions: LogOptions = {runtime_label: 'AXIOS', dumpables}
     logger.log('error', 'Test Message', logOptions)
-    
+
     expect(formatter).toHaveBeenLastCalledWith('axiosRequest', axiosRequestMock, 'error')
     expect(winstonLoggerMock.log.mock.calls[0][2]).toHaveProperty('dumpables', {axiosRequest: axiosRequestMock}) // removes the array when singular
   })
-  
+
   it('overloads the log method correctly', () => {
     logger.log('error', 'Test Message')
-    
+
     expect(winstonLoggerMock.log.mock.calls[0][2]).toEqual({})
   })
-  
+
   it.only('handles grpc errors and just re-logs with message', () => {
     // e.g. - https://goo.gl/8NPfKG
     const logMsg = 'Stupidly large log message'
     const grpcErrorMsg = 'Log entry with size 175.9K exceeds maximum size of 110.0K'
-    winstonLoggerMock.log.mockImplementationOnce(() => {throw new Error(grpcErrorMsg)})
-    logger.log('debug', logMsg, {runtime_label: 'AXIOS', dumpables: {response: ['stupidly large data bit, for instance']}})
+    winstonLoggerMock.log.mockImplementationOnce(() => {
+      throw new Error(grpcErrorMsg)
+    })
+    logger.log('debug', logMsg, {
+      runtime_label: 'AXIOS',
+      dumpables: {response: ['stupidly large data bit, for instance']},
+    })
 
-    expect(winstonLoggerMock.log).toHaveBeenLastCalledWith('error', `Error during log provider call. Original msg: ${logMsg}. Provider error msg: ${grpcErrorMsg}.`)
+    expect(winstonLoggerMock.log).toHaveBeenLastCalledWith(
+      'error',
+      `Error during log provider call. Original msg: ${logMsg}. Provider error msg: ${grpcErrorMsg}.`
+    )
   })
 })
