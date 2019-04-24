eeadcfe133da1634fbedcbcbbd3a7e0412b0e174
Matt Penrice <matthew.penrice@gmail.com>
2019-04-24 11:57:50 +0100

    WIP: typescripts fix
    affects: partridge-logging

--- lib/format.d.ts
+++ lib/format.d.ts
@@ -1,4 +1,3 @@
-import { Dumpable } from './__types__';
-declare const format: (dumpableKey: string, dumpable: Dumpable, logLevel: LogLevel) => any;
+declare const format: (dumpableKey: string, dumpable: any, logLevel: any) => any;
 export default format;
 //# sourceMappingURL=format.d.ts.map
