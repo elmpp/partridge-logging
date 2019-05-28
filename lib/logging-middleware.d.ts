085bbb277ca7c557dbddf16c9b99c92ab17575e1
Matt Penrice <matthew.penrice@gmail.com>
2019-05-28 10:09:15 +0100

    WIP: cloud build
    affects: docs, partridge-importer, partridge-logging

--- lib/logging-middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-declare const logging: (req: express.Request, res: express.Response, next: express.NextFunction) => void;
-declare const applyMiddleware: ({ app }: {
-    app: express.Application;
-}) => void;
-export { applyMiddleware as apply, logging as middleware, };
-//# sourceMappingURL=logging-middleware.d.ts.map
